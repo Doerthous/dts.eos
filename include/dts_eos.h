d842bcee387bf6d9b72e8de1813b4ba11f64c487
Doerthous <827484343@qq.com>
2021-01-29 10:30:32 +0800

    feat: rtthread thread interface

---
+++ include/dts_eos.h
@@ -0,0 +1,42 @@
+/*
+   The MIT License (MIT)
+
+   Copyright (c) 2021 Doerthous
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+
+   Authour: Doerthous <doerthous@gmail.com>
+*/
+
+#ifndef DTS_EOS_H_
+#define DTS_EOS_H_
+
+#include <dts_eos_conf.h>
+#include <dts_eos_t.h>
+
+void dts_eos_thrd_new(dts_eos_thrd_t **thrd, void (*func)(void *), void *param, size_t stack_size);
+void dts_eos_thrd_delete(dts_eos_thrd_t *thrd);
+void dts_eos_thrd_suspend(dts_eos_thrd_t *thrd);
+void dts_eos_thrd_resume(dts_eos_thrd_t *thrd);
+
+#ifdef DTS_EOS_IMPL
+# include <dts_eos_impl.h>
+#endif // DTS_EOS_IMPL_H_
+
+#endif // DTS_EOS_H_
---
+++ rtos/rtthread/dts_eos_impl.h
@@ -0,0 +1,50 @@
+/*
+   The MIT License (MIT)
+
+   Copyright (c) 2021 Doerthous
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+
+   Authour: Doerthous <doerthous@gmail.com>
+*/
+
+#ifndef DTS_EOS_IMPL_H_
+#define DTS_EOS_IMPL_H_
+
+static inline void
+dts_eos_thrd_new(dts_eos_thrd_t **thrd, void (*func)(void *), void *param, size_t stack_size)
+{
+    *thrd = rt_thread_create("", func, param, stack_size,
+        DTS_EOS_THREAD_DEFAULT_PRIORITY, DTS_EOS_THREAD_DEFAULT_TICK);
+    if (*thrd) {
+        rt_thread_startup(*thrd);
+    }
+}
+
+#define dts_eos_thrd_delete rt_thread_delete
+
+#define dts_eos_thrd_suspend(thrd) do \
+{ \
+    rt_thread_suspend(thrd); \
+    rt_schedule(); \
+} while (0)
+
+#define dts_eos_thrd_resume rt_thread_resume
+
+#endif // DTS_EOS_IMPL_H_
